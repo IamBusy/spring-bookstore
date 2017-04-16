e982793d740d8d788d917c2bd3dbe91b8d6fda77
william wei <1342247033@qq.com>
2017-04-16 17:05:31 +0800

    support for transaction

--- src/main/java/com/william/Application.java
+++ src/main/java/com/william/Application.java
@@ -9,10 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {
